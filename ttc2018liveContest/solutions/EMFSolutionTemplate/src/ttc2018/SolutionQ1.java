d90bb7f7dd6d5e9fd6c1b3b2a1445a43f1f3dfc5
maveist <maveist44@gmail.com>
2018-11-20 17:31:56 +0100

    init work q1

--- ttc2018liveContest/solutions/EMFSolutionTemplate/src/ttc2018/SolutionQ1.java
+++ ttc2018liveContest/solutions/EMFSolutionTemplate/src/ttc2018/SolutionQ1.java
@@ -1,32 +1,64 @@
 package ttc2018;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 
-import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.common.util.EList;
 
+import Changes.ChangeTransaction;
+import Changes.CompositionListInsertion;
 import Changes.ModelChange;
 import Changes.ModelChangeSet;
 import SocialNetwork.Comment;
 import SocialNetwork.Post;
-import SocialNetwork.Submission;
 import SocialNetwork.User;
-
 
 public class SolutionQ1 extends Solution {
 
+	private HashMap<Integer, Pair<Integer, Post>> mapAllPosts = new HashMap<Integer, Pair<Integer, Post>>();
+	private HashMap<Integer, Integer> mapAllComments = new HashMap<Integer, Integer>();
+	
 	@Override
 	public String Initial() {
 		// TODO Implement Q1
-		EList<Post> listPosts = initLists();
-		System.out.println(Integer.toString(listPosts.size()));
-		MostControversialPostList topList = new MostControversialPostList();
+		EList<Post> listPosts = this.getSocialNetwork().getPosts();
+		List<Pair<Integer, Post>> resultsPosts = new ArrayList<Pair<Integer, Post>>();
+		TreeMap<Pair<Integer, Post>, Post> treeMap = new TreeMap(new PostsComparator());
 		for(Post post: listPosts) {
-			int nbPoints = getNbPointsPost(post);
-			topList.pushPost(post, nbPoints);
+			int pts = countPointPost(post);
+			Pair<Integer, Post> pair = new Pair(pts, post);
+			mapAllPosts.put(Integer.parseInt(post.getId()), pair);
+			treeMap.put(pair, post);
+			if(treeMap.size() > 3) {
+				treeMap.pollFirstEntry();
+			}
 		}
-		return topList.getMostContreversialPosts();
+		List<Post> ll = new ArrayList(treeMap.values());
+		return ll.get(0).getId()+"|"+ll.get(1).getId()+"|"+ll.get(2).getId();
+		
+	}
+	
+	public int countPointPost(Post post) {
+		int pts = 0;
+		for(Comment comment: post.getComments()) {
+			pts += countPointComment(comment, Integer.parseInt(post.getId()));
+		}
+		return pts;
+	}
+	
+	public int countPointComment(Comment comment, int post) {
+		mapAllComments.put(Integer.parseInt(comment.getId()), post);
+		int pts = 10 + comment.getLikedBy().size();
+		for(Comment subComment: comment.getComments()) {
+			pts += countPointComment(subComment, post);
+		}
+		return pts;
 	}
 
 	@Override
@@ -38,98 +70,56 @@ public class SolutionQ1 extends Solution {
 		// TODO Implement Q1
 		return null;
 	}
-	
-	public EList<Post> initLists() {
-		EList<Post> listPosts = this.getSocialNetwork().getPosts(); 
-		return listPosts;
-	}
-	
-	public int getNbPointsPost(Post post) {
-		int nbComment = 0;
-		HashSet<User> likerSet = new HashSet<User>();
-		for(Comment comment : post.getComments()) {
-			Pair<Integer, HashSet<User>> res = getNbCommentAndLike(comment);
-			nbComment += res.getFirst();
-			likerSet.addAll(res.getSecond());
-		}
-		return nbComment*10 + likerSet.size();
-	}
-	
-	public Pair<Integer, HashSet<User>> getNbCommentAndLike(Comment comment) {
-		int nbComment = 1;
-		HashSet<User> likerSet = new HashSet<User>();
-		for(Comment subComment: comment.getComments()) {
-			Pair<Integer, HashSet<User>> res = getNbCommentAndLike(subComment);
-			nbComment += res.getFirst();
-			likerSet.addAll(res.getSecond());
-		}
-		return new Pair<Integer, HashSet<User>>(nbComment, likerSet);
-	}
-	
-	public class MostControversialPostList{
-		
-		private Pair<Integer, Post>[] posts;
-		public MostControversialPostList() {
-			posts = new Pair[3];
-		}
-		
-		//i<=1 && posts[i+1].getSecond().getTimestamp().compareTo(posts[i].getSecond().getTimestamp()) == -1
-		
-		public void pushPost(Post p, int nbPoints) {
-			Pair<Integer, Post> newPair = new Pair(nbPoints, p);
-			for(int i = 0; i<3;i++) {
-				if(posts[i] == null) {
-					posts[i] = newPair;
-					break;
-				}else if(posts[i].getFirst() < nbPoints && isFull()){
-					posts[i] = newPair;
-					break;
-				}else if(posts[i].getFirst() < nbPoints && !isFull()) {
-					posts[i+1] = posts[i];
-				}else if(posts[i].getFirst() == nbPoints && isFull()) {
-					if(p.getTimestamp().compareTo(posts[i].getSecond().getTimestamp()) == 1) {
-						Pair tmp = posts[i];
-						for(int j=i; j<2; j++) {
-							Pair tmp1 = posts[j+1];
-							posts[j+1] = tmp;
-							tmp = tmp1;
-						}
-						posts[i] = newPair;
-						break;
-					}
-				}else if(posts[i].getFirst() == nbPoints && !isFull()) {
-					if(p.getTimestamp().compareTo(posts[i].getSecond().getTimestamp()) == 1) {
-					int k = i;
-					Pair tmp = posts[i];
-					for(int j=i; j<nbElem()-1; j++) {
-						Pair tmp1 = posts[j+1];
-						posts[j+1] = tmp;
-						tmp = tmp1;
-					}
-					posts[k] = newPair;					
-					break;
-					}
-				}
-			}
-		}
-		
-		public int nbElem() {
-			int cpt = 0;
-			for(int i=0; i<3; i++) {
-				if(posts[i] != null) {
-					cpt++;
-				}
-			}
-			return cpt;
-		}
-		
-		public boolean isFull() {
-			return nbElem() == 3;
-		}
-		
-		public String getMostContreversialPosts() {
-			return posts[0].getSecond().getId()+"|"+posts[1].getSecond().getId()+"|"+posts[2].getSecond().getId();
-		}
-	}
 
+
+	public void updatePoints(ModelChange m) {
+		if(m.eClass().getName().equals("ChangeTransaction")) {
+			ChangeTransaction change = (ChangeTransaction) m;
+			if(change.getSourceChange().eClass().getName().equals("CompositionListInsertion")) {
+				CompositionListInsertion sourceChange = (CompositionListInsertion) change.getSourceChange();
+				if(sourceChange.getAffectedElement().eClass().getName().contentEquals("Post")) {
+					Post post = (Post) sourceChange.getAffectedElement();
+					Pair<Integer, Post> pair = mapAllPosts.get(Integer.parseInt(post.getId()));
+					pair.setFirst(pair.getFirst() + 10);
+				}
+			}
+		}	
+		
+	}
+	
+	public void updatePoints(CompositionListInsertion change) {
+		if(change.getAffectedElement().eClass().getName().equals("Post")) {
+			Post post = (Post) change.getAffectedElement();
+			Pair<Integer, Post> pair = mapAllPosts.get(Integer.parseInt(post.getId()));
+			pair.setFirst(pair.getFirst() + 10);
+		}else if(change.getAffectedElement().eClass().getName().equals("Comment")) {
+			Pair<Integer, Post> pair = mapAllPosts.get(
+					mapAllComments.get(
+							Integer.parseInt(((Comment) change.getAffectedElement()).getId())
+					)
+			);
+			
+			pair.setFirst(pair.getFirst()+10);
+		}
+	}
+	
+	public class PostsComparator implements Comparator<Pair<Integer, Post>>{
+
+		@Override
+		public int compare(Pair<Integer, Post> o1, Pair<Integer, Post> o2) {
+			// TODO Auto-generated method stub
+			
+			if(o1.getFirst() > o2.getFirst()) {
+				return 1;
+			}else if(o1.getFirst() < o2.getFirst()){
+				return -1;
+			}else {
+				return o1.getSecond().getTimestamp().compareTo(o2.getSecond().getTimestamp());
+			}
+
+		}
+
+		
+		
+	}
 }
